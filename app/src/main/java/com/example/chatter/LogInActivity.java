f0e0a0bef517fc2e1067c0b175fd157ea5279993
Ather-Ahmad-Dev <atherahmadwork@gmail.com>
2024-08-09 15:31:55 -0700

    UX: Keyboard Overlay, Hided System Navigation Bar

--- app/src/main/java/com/example/chatter/LogInActivity.java
+++ app/src/main/java/com/example/chatter/LogInActivity.java
@@ -2,6 +2,7 @@ package com.example.chatter;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.activity.EdgeToEdge;
@@ -55,6 +56,20 @@ public class LogInActivity extends AppCompatActivity {
             public void onClick(View view) {
                 intent = new Intent(LogInActivity.this, SignUpActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        binding.main.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                View decorView = getWindow().getDecorView();
+                decorView.setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_IMMERSIVE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                );
+                return false;
             }
         });
     }
